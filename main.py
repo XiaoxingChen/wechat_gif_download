00e3094dfae678d1e2a2b2c6c2aef14895a02884
XiaoxingChen <chen-xx@zju.edu.cn>
2021-12-25 23:46:03 +0800

    Add: image type recognition.

--- main.py
+++ main.py
@@ -4,22 +4,42 @@ import re
 import os
 import shutil
 
+img_counter = 0
+
+def checkImageType(filename):
+    with open(filename, 'rb') as f:
+        s = f.read()
+        if s[0:4] == b'\xff\xd8\xff\xe0':
+            return 'jpg'
+        if s[0:3] == b'GIF':
+            return 'gif'
+        if s[0:4] == b'\x89PNG':
+            return 'png'
+        return 'txt'
+
 @itchat.msg_register(INCOME_MSG)
 def text_reply(msg):
     pattern = 'cdnurl = "(.*?)"'
-    # msg.Content
-    # print("msg.Content: {}".format(msg.Content))
+
     if msg.MsgType == 47:
+        # print("msg.Content: {}".format(msg.Content))
         url = re.search(pattern, msg.Content).groups()[0]
-        url = url.replace("&amp;", "&")
-        if "&amp;" in url:
-            url = url.replace("&amp;", "&")
+        # url = url.replace("&amp;", "&")
+        cache_name = ''
+        if "stodownload" in url:
+            cache_name = 'stodownload'
+            url = url.replace("&amp;", "%26")
+            url = url.replace("?", "%3F")
             print(url)
+            return
         else:
+            cache_name = 'index.html'
             print(url)
-            os.system('wget {}'.format(url))
-            target_filename = url.split('/')[-2] + '.gif'
-            shutil.move('index.html', os.path.join('pics', target_filename))
+        os.system('wget {}'.format(url))
+
+        img_type = checkImageType(cache_name)
+        target_filename = str(img_counter) + '.' + img_type
+        shutil.move(cache_name, os.path.join('pics', target_filename))
 
 if not os.path.isdir('pics'):
     os.mkdir('pics')
